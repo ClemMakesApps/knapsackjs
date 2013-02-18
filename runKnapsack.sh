0859818fe71aadcf65ebeb4e0be2db3ea8eda09f
Clement Ho <chh09a@acu.edu>
2013-02-18 15:46:16 -0600

    Updated knapsack sh script w/ colors

--- runKnapsack.sh
+++ runKnapsack.sh
@@ -1,4 +1,18 @@
 #! /bin/sh
+echo "\033[32m=== data/classData/k25.json ===\033[0m"
 node main.js data/classData/k25.json 0
+echo "\033[33m============================\033[0m"
 node main.js data/classData/k25.json 1
+echo "\033[33m============================\033[0m"
 node main.js data/classData/k25.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k25.json 3
+echo "\n\033[32m=== data/classData/k20.json ===\033[0m"
+node main.js data/classData/k20.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k20.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k20.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k20.json 3
+echo "\033[32m============================\033[0m"
