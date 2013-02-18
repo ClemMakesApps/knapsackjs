17dac334b734b97547bb64573cc90a02440a3b22
Clement Ho <chh09a@acu.edu>
2013-02-18 16:17:53 -0600

    sh script now calls all classData

--- runKnapsack.sh
+++ runKnapsack.sh
@@ -1,12 +1,28 @@
 #! /bin/sh
-echo "\033[32m=== data/classData/k25.json ===\033[0m"
-node main.js data/classData/k25.json 0
+echo "\033[32m=== data/classData/k05.json ===\033[0m"
+node main.js data/classData/k05.json 0
 echo "\033[33m============================\033[0m"
-node main.js data/classData/k25.json 1
+node main.js data/classData/k05.json 1
 echo "\033[33m============================\033[0m"
-node main.js data/classData/k25.json 2
+node main.js data/classData/k05.json 2
 echo "\033[33m============================\033[0m"
-node main.js data/classData/k25.json 3
+node main.js data/classData/k05.json 3
+echo "\n\033[32m=== data/classData/k10.json ===\033[0m"
+node main.js data/classData/k10.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k10.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k10.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k10.json 3
+echo "\n\033[32m=== data/classData/k15.json ===\033[0m"
+node main.js data/classData/k15.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k15.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k15.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k15.json 3
 echo "\n\033[32m=== data/classData/k20.json ===\033[0m"
 node main.js data/classData/k20.json 0
 echo "\033[33m============================\033[0m"
@@ -15,4 +31,36 @@ echo "\033[33m============================\033[0m"
 node main.js data/classData/k20.json 2
 echo "\033[33m============================\033[0m"
 node main.js data/classData/k20.json 3
+echo "\n\033[32m=== data/classData/k25.json ===\033[0m"
+node main.js data/classData/k25.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k25.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k25.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k25.json 3
+echo "\n\033[32m=== data/classData/k30.json ===\033[0m"
+node main.js data/classData/k30.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k30.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k30.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k30.json 3
+echo "\n\033[32m=== data/classData/k35.json ===\033[0m"
+node main.js data/classData/k35.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k35.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k35.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k35.json 3
+echo "\n\033[32m=== data/classData/k40.json ===\033[0m"
+node main.js data/classData/k40.json 0
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k40.json 1
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k40.json 2
+echo "\033[33m============================\033[0m"
+node main.js data/classData/k40.json 3
 echo "\033[32m============================\033[0m"
